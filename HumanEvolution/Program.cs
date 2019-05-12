a90567d8d3a1f4562010a53a494f90c3677ac88c
SyntaxCheck <justin.wolbeck@gmail.com>
2019-05-12 12:19:51 -0500

    Remove comment, test

--- HumanEvolution/Program.cs
+++ HumanEvolution/Program.cs
@@ -8,9 +8,6 @@ namespace MGTemplate
     /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
